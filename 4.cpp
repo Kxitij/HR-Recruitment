a373419feb2f58040fa636f8bebd78f08ef04a4e
Kxitij <kshitij.coolkarni@gmail.com>
2023-03-08 14:32:59 +0530

    changed logic, removed classes, and added read write functons to the main method. Now the soting is done based on the alphabetical order of the designation. Have to change it in a way that we prioritise the designations.

--- 4.cpp
+++ 4.cpp
@@ -1,114 +1,69 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
-
 using namespace std;
 
-struct emp
-{
+struct Employee {
     string name;
-    string desig;
-    emp* next;
+    string designation;
+    Employee* next;
 };
-class HR{
-    private: 
-        emp* head;
-    public: 
-        HR(){
-            head = NULL;
-        }
-        void insertEMP(string name, string designation){
-            emp* temp = new emp();
-            temp->name = name;
-            temp->desig = designation;
-            temp->next = head;
-            if (head == NULL){ head = temp; return; }
-            emp* ptr = head;
-            while (ptr->next!= NULL){
-                ptr = ptr->next;
-            }
-            ptr->next = temp;
-        }
 
-        void display(){
-            emp* current = head;
-            while (current!= NULL){
-                cout<<"Name: "<<current->name<<"\tDesignation:"<<current->desig<<endl;
-                current = current->next;
-            }
-            cout<<endl;
-        }
-        
-        void swap(emp* a, emp* b) {
-            string temp_name = a->name;
-            string temp_desig = a->desig;
-            a->name = b->name;
-            a->desig = b->desig;
-            b->name = temp_name;
-            b->desig = temp_desig;
-        }
-        void sort() {
-            emp* curr;
-            emp* last = NULL;
-            bool swapped;
 
-            
-            if (head == NULL) {
-                return;
-            }
-
-            do {
-                swapped = false;
-                curr = head;
-
-                while (curr->next != last) {
-                    // compare the lengths of the designations first
-                    if (curr->desig.compare(curr->next->desig) > 0) {
-                        swap(curr, curr->next);
-                        swapped = true;
-                    }
-                    curr = curr->next;
-                }
-                last = curr;
-            } while (swapped);
+void insert(Employee*& head, string name, string designation) {
+    Employee* newNode = new Employee;
+    newNode->name = name;
+    newNode->designation = designation;
+    newNode->next = nullptr;
+    
+    if (head == nullptr) {
+        head = newNode;
+    } else {
+        Employee* current = head;
+        Employee* prev = nullptr;
+        while (current != nullptr && current->designation < designation) {
+            prev = current;
+            current = current->next;
         }
-        void readFile(string filename){
-    string line;
-    ifstream in(filename);
-    if (!in){
-        cout<<"File not found"<<endl;
-        return;
-    }
-    while (getline(in, line)){
-        size_t pos = 0;
-        pos = line.find(',');
-        if (pos != string::npos){
-            string name = line.substr(0,pos);
-            string designation = line.substr(pos+1);
-            insertEMP(name, designation);
+        if (prev == nullptr) {
+            newNode->next = head;
+            head = newNode;
+        } else {
+            prev->next = newNode;
+            newNode->next = current;
         }
     }
 }
 
-        void writeIntoFile(string filename){
-            ofstream out(filename);
-            if (!out){
-                cout<<"File not found"<<endl;
-                return;
-            }
-            for (emp* curr = head; curr!= NULL; curr = curr->next) {
-                out << curr->name << " " << curr->desig << endl;
-            }
-            cout<<"Data entered to the file."<<endl;
-            out.close();
-        } 
-};
 
-int main(){
-    HR h;
-    h.readFile("employee.txt");
-    h.sort();
-    h.writeIntoFile("output.txt");
+void print(Employee* head) {
+    Employee* current = head;
+    while (current != nullptr) {
+        cout << current->name << " - " << current->designation << endl;
+        current = current->next;
+    }
+}
+
+
+void saveToFile(Employee* head, string filename) {
+    ofstream outFile(filename);
+    Employee* current = head;
+    while (current != nullptr) {
+        outFile << current->name << "," << current->designation << endl;
+        current = current->next;
+    }
+    outFile.close();
+}
+
+int main() {
+    Employee* head = nullptr;
+    ifstream inFile("employees.txt");
+    string name, designation;
+    while (getline(inFile, name, ',') && getline(inFile, designation)) {
+        insert(head, name, designation);
+    }
+    inFile.close();
+    print(head);
+    saveToFile(head, "sorted_employees.txt");
     return 0;
 }
